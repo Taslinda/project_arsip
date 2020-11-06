566c322b7a5796e40201624cc0bc75e626279a47
Taslinda <taslinda.latif@gmail.com>
2020-11-06 13:47:36 +0800

    edit-layouts

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,6 @@
 <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
-    All rights reserved.
+    <strong> &copy; 2020 <a href="http://adminlte.io">DISPENDA</a>.</strong>
     <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 3.0.5
+      <b>Pengarsipan Berkas Pajak</b> 
     </div>
   </footer>
